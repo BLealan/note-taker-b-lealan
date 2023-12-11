e42c9b30bd4b3a9d8efcb339df5472a114c68f3d
BLealan <143086275+BLealan@users.noreply.github.com>
2023-12-11 21:16:24 +0000

    add note functionality

--- routes/notes.js
+++ routes/notes.js
@@ -1,8 +1,12 @@
 const notes = require("express").Router();
 
 notes.get("/", (req, res) => {
+    readFromFile(".db/notes.json").then((data) => res.json(JSON.parse(data)));
+});
+
+notes.get("/:note_id", (req, res) => {
     const noteID = req.params.note_id;
-    readFromFile("./db/notes.json")
+    readFromFile("./db/db.json")
     .then((data) => JSON.parse(data))
     .then((json) => {
         const result = json.filter((note) => note.note_id === noteId);
@@ -11,3 +15,32 @@ notes.get("/", (req, res) => {
         : res.json("No notes");
     });
 });
+
+notes.delete("/:note_id", (req, res => {
+    const noteID = req.params.note_id;
+    readFromFile("./db/db.json")
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+        const result = json.filter((note) => note.note_id !== noteID);
+        writeToFile("./db/db.json", result);
+        res.json(`Note ${noteID} has been deleted!`)
+    });
+}));
+
+notes.post("/", (req, res) => {
+    const { title, text } = req.body;
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+        };
+
+        readAndAppend(newNote, "./db/tips.json");
+        res.json("Note added successfully");
+    } else {
+        res.errored("Error in creating new note");
+    }
+});
+
+module.exports = notes;
