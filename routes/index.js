c2d95b8584ff6a7c57d9200185acfe0b4faf266a
BLealan <143086275+BLealan@users.noreply.github.com>
2023-12-13 18:44:37 +0000

    README content

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 
 const notesRouter = require('./notes');
 
-router.use('/tips', notesRouter);
+router.use('/notes', notesRouter);
 
 module.exports = router;
--- routes/notes.js
+++ routes/notes.js
@@ -1,5 +1,6 @@
 const notes = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
+const fs = require ('fs');
 
 notes.get('/', (req, res) => {
     readFromFile('.db/db.json').then((data) => res.json(JSON.parse(data)));
